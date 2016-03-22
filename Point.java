e8ce9946dc3ba10151631470b81a4df146e53f6e
ArikVhedrovix <efedrowisch@yahoo.com>
2016-03-21 21:58:07 -0500

    Parser code now parses
    -Parser functionality now works.
    -Points now properly store their instance variables

--- Point.java
+++ Point.java
@@ -1,12 +1,12 @@
 package TSP;
 public class Point{
   public double x,y;
-  public int n;
+  public int node;
   
   public Point(int n, double a, double b){
-    int node = n; 
-    double x = a;
-    double y = b;
+    this.node = n; 
+    this.x = a;
+    this.y = b;
   }
 
 }
--- TTSP.java
+++ TTSP.java
@@ -13,37 +13,31 @@ public class TTSP {
   public TTSP(String fileName)
   {
    ArrayList<Point> points = this.parse(fileName);
+    for (Point p: points) {System.out.println(p.x+","+p.y);}
+    System.out.println("# of Points: "+ points.size());
   }
   
   public ArrayList<Point> parse(String file)
   {
-   ArrayList<Point> p = new ArrayList<Point>(300);
+   ArrayList<Point> p = new ArrayList<Point>();
    String line = null; //For readLine()
    String pattern = "(\\d+)(.+)(\\d+)(.+)(\\d+)";
    String[] match;
    Pattern regex = Pattern.compile(pattern);
    try {
       BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-      int c = 0;
       while((line = bufferedReader.readLine()) != null) {
-        System.out.println(line);
+        //System.out.println(line);
         Matcher m = regex.matcher(line);
         if (m.find()) {
-          c++;
-          match=m.group(0).split("( )");
+          match=m.group(0).split("(\\s+)");
           int n = Integer.parseInt(match[0]);
           double a = Double.parseDouble(match[1]);
           double b = Double.parseDouble(match[2]);
-          System.out.println(n+","+a+","+b);
-          /*
-          this.points.add(new Point(Integer.parseInt(match[0]),
-                                    Double.parseDouble(match[1]),
-                                    Double.parseDouble(match[2])
-                    ));*/
-          //for (String s : match){System.out.println(s);}
+          //System.out.println(n+","+a+","+b);
+          p.add(new Point(n,a,b));
         }
       }
-      System.out.println("Found: " + c);
       bufferedReader.close();         
     }//End of try
     catch(FileNotFoundException e){
